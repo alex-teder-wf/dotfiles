f69ef3b22e1daa9cee15a1bba81793abfab7817a
Aleksandr Teder <aleksandr.teder@wayflyer.com>
2026-08-02 16:33:28 +0400

    wip

--- dotfiles/.bashrc.d/dotfiles.sh
+++ dotfiles/.bashrc.d/dotfiles.sh
@@ -1,3 +1,6 @@
 export PATH="/opt/wf-state/neovim/bin:$PATH"
 alias v="nvim"
 export CURSOR_CONFIG_DIR=/opt/wf-state/.config/cursor
+eval "$(starship init bash)"
+eval "$(zoxide init bash)"
+export PATH="/root/.cargo/bin:$PATH"
