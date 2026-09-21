15fa0894b946d86c2d405c9f629d922463e27271
Aleksandr Teder <aleksandr.teder@wayflyer.com>
2026-09-21 13:10:28 +0400

    add thing

--- dotfiles/.bashrc.d/dotfiles.sh
+++ dotfiles/.bashrc.d/dotfiles.sh
@@ -1,6 +1,7 @@
 export PATH="/opt/wf-state/neovim/bin:$PATH"
 alias v="nvim"
 alias t="tmux a || tmux"
+alias ':G'='git status --short --branch'
 export CURSOR_CONFIG_DIR=/opt/wf-state/.config/cursor
 eval "$(starship init bash)"
 eval "$(zoxide init bash)"
